95f59d5bdb2c0da4475deea3502880b67883a26e
Chitranshi1~ <chitranshigourana@gmail.com>
2024-06-11 10:33:05 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db_config');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+
+
+// DB Connection
+connectDB();
+
+// body Pareser
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+app.get('/', (req,res)=>{
+  res.json({
+    message : "WELCOME TO THE TASK TRACKER API",
+  });
+});
+
+// todo routes
+app.use('/api/todo', require('./routes/todo/todoRoutes'));
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running at PORT : ${PORT}`.bgBlue.white);
+})
